47539d6c1e29b10f0ea47e0da167d8cb5dd03208
Alvaro Japa Salazar <alvaruwiiu@hotmail.com>
2022-10-30 10:24:08 -0500

    ordenamiento shellsort

--- shellsort.py
+++ shellsort.py
@@ -1,14 +1,19 @@
-def shellsort(arr):
-    size = len(arr)
-    gap = size // 2
-    while gap > 0:
-        for i in range(gap, size):
-            anchor = arr[i]
-            j = i
-            while j >= gap and arr[j-gap] > anchor:
-                arr[j] = arr[j-gap]
-                j -= gap
-            arr[j] = anchor
-        gap = gap // 2
+def shellsort(array):
+    length = len(array)
+    interval = length // 2
+    while interval > 0:
+        for i in range(interval, length):
+            insert_value = array[i]
+            insert_index = i
+            
+            while insert_index >= interval and array[insert_index - interval] > insert_value:
+                array[insert_index] = array[insert_index - interval]
+                insert_index -= interval
+            
+            array[insert_index] = insert_value
+        interval //= 2
+    return array
+
+        
 
 print(shellsort([8,12,1,3,5]))
