eff1efe0b7dd3ba5903831eb6581376ab0ed352c
Alvaro Japa Salazar <alvaruwiiu@hotmail.com>
2022-10-30 12:23:15 -0500

    prueba de escritorio merge

--- mergesort.py
+++ mergesort.py
@@ -31,3 +31,34 @@ def merge_sort(lista):
     return merge(left, right)
 
 print(merge_sort([8,12,1,3,5]))
+# como funciona
+"""
+dividira la lista de forma entera en 2 listas
+[8,12,1,3,5]
+isquierda [8, 12]
+derecha  [1,3,5] 
+
+Izquierda
+    vuelve a dividir la lista izquierda en 2
+    [8] [12]
+    las compara quien es meno y las vuelva a insertar en una nueva lista
+    [8, 12]
+Derecha
+    vuelve a dividir entre 2 la lista de la derecha dando como resultado
+    [1]
+    [3,5]
+    como aun queda una lista con mas de 1 elemento volvera a dividir teniendo como resultado
+    [1][3][5]
+    comparara  el y 3 y lo ordenara en una nueva lista
+    [1 , 3 ]
+    luego hara la comparacion  con el elmento 5 dando como resultado
+    [1, 3, 5]
+    
+una vez vuelto a crear estas 2 listas se compararan y ordenaran en una nueva lista
+[8, 12] y [1, 3, 5]
+dando como resultado 
+[1, 3, 5, 8 ,12]
+    
+
+
+"""
